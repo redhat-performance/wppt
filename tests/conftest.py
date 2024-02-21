1fbb57e71b6b826889fe7910b8f59c65ece3a34a
Gonzalo Rafuls <grafuls@gmail.com>
2024-02-21 23:17:38 +0100

    tests: added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,12 @@
-"""Integration tests configuration file."""
+import pytest
 
-# pylint: disable=unused-import
+from wppt.app import app as flask_app
+
+@pytest.fixture(scope="module")
+def test_client():
+    """
+    | Creates a test client for the app.
+    """
+    with flask_app.test_client() as testing_client:
+        with flask_app.app_context():
+            yield testing_client
